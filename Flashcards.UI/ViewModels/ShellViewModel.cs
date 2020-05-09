a7abdfe9b288337bf5cf65c6a4152acd729f8f4d
Michał Banach <michal_banach@outlook.com>
2020-05-09 19:58:56 +0200

    Add simple UI form.

---
+++ Flashcards.UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Caliburn.Micro;
+
+namespace Flashcards.UI.ViewModels
+{
+    class ShellViewModel : Screen
+    {
+        public string Question { get; set; } = "Co to za pytanie?";
+        public string Answer {get; set; } = "Odpowiedź";
+
+        public void Enter(string answer)
+        {
+
+        }
+    }
+}

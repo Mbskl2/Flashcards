414e126317091974b256ad45406770825624ee5e
Michał Banach <michal_banach@outlook.com>
2020-05-10 11:33:03 +0200

    Add AnswerValidator tests.

--- FlashCards/Answers/AnswerValidator.cs
+++ FlashCards/Answers/AnswerValidator.cs
@@ -5,9 +5,22 @@ namespace FlashCards.Answers
 {
     public class AnswerValidator
     {
-        public ValidationResult Validate(IQuestion question)
+        public ValidationResult Validate(IQuestion question, string userAnswer)
         {
-            throw new NotImplementedException();
+            var qa = question as QuestionWithAnswer;
+            if (qa is null)
+                throw new WrongTypeException();
+            if (qa.Answer.Equals(userAnswer, StringComparison.InvariantCulture))
+                return ValidationResult.Correct();
+            return ValidationResult.Failed(qa.Answer);
+        }
+
+        public class WrongTypeException : Exception
+        {
+            public WrongTypeException()
+            : base("The IQuestion for validation has to be the same object that you got from QuestionProvider")
+            {
+            }
         }
     }
 }
---
+++ FlashCardsTests/Answers/AnswerValidatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlashCards.Answers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FlashCards.Questions;
+using FluentAssertions;
+
+namespace FlashCardsTests.Answers
+{
+    [TestClass]
+    public class AnswerValidatorTests
+    {
+        private AnswerValidator validator;
+        private const string question1 = "Question one one two?";
+        private const string answer1 = "Answer1 one one two.";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            validator = new AnswerValidator();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AnswerValidator.WrongTypeException))]
+        public void Throws_When_GivenNotQuestionWithAnswer()
+        {
+            validator.Validate(new Question(), answer1);
+        }
+
+        [TestMethod]
+        public void ReturnCorrectResult_When_AnswerIsCorrect()
+        {
+            var question = new QuestionWithAnswer(question1, answer1);
+            var result = validator.Validate(question, answer1);
+            result.IsCorrect.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ReturnIncorrectResult_When_AnswerIsFalse()
+        {
+            var question = new QuestionWithAnswer(question1, answer1);
+            var result = validator.Validate(question, "Wrong answer.");
+            result.IsCorrect.Should().BeFalse();
+            result.CorrectAnswer.Should().Be(answer1);
+        }
+
+        [TestMethod]
+        public void ReturnIncorrectResult_When_AnswerHasWrongCase()
+        {
+            var question = new QuestionWithAnswer(question1, answer1);
+            var result = validator.Validate(question, answer1.ToLower());
+            result.IsCorrect.Should().BeFalse();
+            result.CorrectAnswer.Should().Be(answer1);
+        }
+
+        private class Question : IQuestion
+        {
+            public string Text { get; }
+        }
+    }
+}
